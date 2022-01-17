c2df1d54cd546975d1568557c8d7d1fe1a56acb3
juanm241 <bra000@gmail.com>
2022-01-17 19:22:05 -0400

    Agregando cambios en la seccion Card

---
+++ src/js/component/jumbotron.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Jumbotron = () => {
+	return <h1>Hola Jumbotron</h1>;
+};
---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Navbar = () => {
+	return <h1> hello Navbar</h1>;
+};
