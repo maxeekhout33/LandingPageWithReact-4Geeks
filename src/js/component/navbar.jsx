9f913a9e231d7e6bc85272666652c72c666df01d
Maximiliano Eekhout <maxeekhout93@gmail.com>
2022-01-17 19:25:24 -0400

    Se carga el Jumbotron

---
+++ src/js/component/navbar.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Navbar = () => {
+	return <p>Hola Navbar</p>;
+};
--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,8 @@ import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
+// import Navbar from "./component/navbar.jsx";
+// import Card from "./component/card.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
