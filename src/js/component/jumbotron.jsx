3eee4c47ed9c917fe77bc67ad4ba9f3e69e6bdb0
Maximiliano Eekhout <maxeekhout93@gmail.com>
2022-01-17 20:43:08 -0400

    Cambios en el jumbotron

--- src/js/component/jumbotron.jsx
+++ src/js/component/jumbotron.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Jumbotron = () => {
 	return (
 		<div>
-			<div className="jumbotron bg-light">
+			<div className="jumbotron bg-light p-5 mb-4 mt-5">
 				<h1 className="display-4">A Warm Welcome!</h1>
 				<p className="lead">
 					This is a simple hero unit, a simple jumbotron-style
